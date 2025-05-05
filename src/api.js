63b318b2be33cb10f4c038df9544c214e97b9017
Nivedita <141390434+Nivedita-Chhokar@users.noreply.github.com>
2025-05-05 14:22:46 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
-const apiInstance = axios.create({
-  baseURL: 'https://eval-backend-lyart.vercel.app/',
-  headers: {
-      'x-access-token': localStorage.getItem('token'  ),
-  }
+const baseURL = "https://eval-backend-lyart.vercel.app";
+
+const api = axios.create({
+  baseURL,
 });
 
-export default apiInstance
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
